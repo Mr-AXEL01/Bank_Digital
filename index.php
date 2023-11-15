8a7f23c8728ce90fbf7d4f4d4821598c445fd754
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2023-11-15 22:51:42 +0100

    add a configure file for tailwindcss and finish styling navbar

--- index.php
+++ index.php
@@ -6,13 +6,19 @@
     <script src="https://cdn.tailwindcss.com"></script>
     <title>BANK APP</title>
 </head>
-<body>
-    <nav class="flex">
-        <div class="navbar-logo">A-Bank</div>
-        <ul>
-            <li><a href="#"></a>customers</li>
-            <li><a href="#"></a>Account</li>
-            <li><a href="#">Transaction</a></li>
+<body class="m-0 p-0 box-border">
+    <nav class="flex justify-around items-center h-20 bg-slate-800 text-white">
+        <a href="index.php" class="navbar-logo text-4xl font-bold">A-Bank</a>
+        <ul class="flex justify-around items-center h-full w-[30%]">
+            <li class="hover:bg-slate-600 p-2 h-full">
+                <a href="#" class="text-xl flex items-center h-full">Customers</a>
+            </li>
+            <li class="hover:bg-slate-600 p-2 h-full">
+                <a href="#" class="text-xl flex items-center h-full">Account</a>
+            </li>
+            <li class="hover:bg-slate-600 p-2 h-full">
+                <a href="#" class="text-xl flex items-center h-full">Transaction</a>
+            </li>
         </ul>
     </nav>
 </body>
