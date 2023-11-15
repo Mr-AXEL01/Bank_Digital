66743cb344e45f239c92750687c93ed9282ccfe5
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2023-11-15 16:49:07 +0100

    check our connection

--- db_connection.php
+++ db_connection.php
@@ -9,6 +9,10 @@ $dbname = "Bank_Digital";
 // creating a connection for our database
 $conn = new mysqli($servername, $username, $password);
 
+// ckecking our connectin .
+if ($conn->connect_error) {
+    die("Connection failed: ". $conn->connect_eroor);
+}
 
 
 
