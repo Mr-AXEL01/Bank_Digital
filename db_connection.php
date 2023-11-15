8db4fca367d2c2ef3f11a4fb4c7700b90f00476b
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2023-11-15 16:47:05 +0100

    start creating a database connection file

---
+++ db_connection.php
@@ -0,0 +1,15 @@
+<!-- creating a php connecting database file (ok so explain it for me to handl database connection from one separate file)  -->
+
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "Bank_Digital";
+
+// creating a connection for our database
+$conn = new mysqli($servername, $username, $password);
+
+
+
+
+?>
