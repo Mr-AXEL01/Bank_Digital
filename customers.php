c62ad695113e0179428a7ebcaa040ede1b52a8e0
abdelhak20azur02@gmail.com <abdelhak20azur02@gmail.com>
2023-11-16 02:15:19 +0100

    create customers page and insert our navbar and footer

---
+++ customers.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://cdn.tailwindcss.com"></script>
+    <title>BANK APP</title>
+</head>
+<body class="m-0 p-0 box-border">
+    <nav class="flex justify-around items-center h-20 bg-slate-800 text-white">
+        <a href="index.php" class="navbar-logo text-4xl font-bold">A-Bank</a>
+        <ul class="flex justify-around items-center h-full w-[30%]">
+            <li class="hover:bg-slate-600 p-2 h-full">
+                <a href="#" class="text-xl flex items-center h-full">Customers</a>
+            </li>
+            <li class="hover:bg-slate-600 p-2 h-full">
+                <a href="#" class="text-xl flex items-center h-full">Account</a>
+            </li>
+            <li class="hover:bg-slate-600 p-2 h-full">
+                <a href="#" class="text-xl flex items-center h-full">Transaction</a>
+            </li>
+        </ul>
+    </nav>
+    
+    <footer class="text-center p-4 bg-slate-800 text-white">
+        <p>&copy; 2023 Bank App</p>
+    </footer>
+</body>
+</html>
+
+
