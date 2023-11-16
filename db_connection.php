7538add1c3c219b96b1f74a9db3f8befca4fb915
abdelhak20azur02@gmail.com <abdelhak20azur02@gmail.com>
2023-11-16 03:59:56 +0100

    fix error in connection page

--- db_connection.php
+++ db_connection.php
@@ -12,34 +12,58 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-// select our database 
+// Drop the existing database
+$dropDatabaseSql = "DROP DATABASE IF EXISTS $dbname";
+if ($conn->query($dropDatabaseSql) === TRUE) {
+    echo "Database dropped successfully";
+} else {
+    echo "Error dropping database: " . $conn->error;
+}
+
+// Create a new database
+$sql = "CREATE DATABASE IF NOT EXISTS $dbname";
+if ($conn->query($sql) === TRUE) {
+    echo "Database created successfully";
+} else {
+    echo "Error creating database: " . $conn->error;
+}
+
+// Select the database
 $conn->select_db($dbname);
 
-// Create customers table if not exists
-$sql = "CREATE TABLE IF NOT EXISTS customers (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
-    email VARCHAR(255) NOT NULL,
-    nationality VARCHAR(50),
-    gender VARCHAR(10),
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-)";
+// Check if the customers table exists
+$tableCheckSql = "SHOW TABLES LIKE 'customers'";
+$tableCheckResult = $conn->query($tableCheckSql);
 
-if ($conn->query($sql) === TRUE) {
-    echo "Customers table created successfully<br>";
-} else {
-    echo "Error creating customers table: " . $conn->error;
+if ($tableCheckResult->num_rows == 0) {
+    // Create customers table if it doesn't exist
+    $createTableSql = "CREATE TABLE customers (
+        id INT AUTO_INCREMENT PRIMARY KEY,
+        name VARCHAR(255) NOT NULL,
+        email VARCHAR(255) NOT NULL,
+        nationality VARCHAR(50),
+        gender VARCHAR(10),
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    )";
+
+    if ($conn->query($createTableSql) === TRUE) {
+        echo "Customers table created successfully";
+        
+        // Insert sample data into the customers table (only if the table is created for the first time)
+        $insertDataSql = "INSERT INTO customers (name, email, nationality, gender) VALUES
+            ('Rayan Fiache', 'rayan@example.com', 'Morocco', 'Male'),
+            ('Jane Doe', 'jane@example.com', 'Canada', 'Female')";
+
+        if ($conn->query($insertDataSql) === TRUE) {
+            echo "Sample data inserted successfully";
+        } else {
+            echo "Error inserting sample data: " . $conn->error;
+        }
+    } else {
+        echo "Error creating customers table: " . $conn->error;
+    }
 }
 
-// Insert sample data into the customers table
-$insertDataSql = "INSERT INTO customers (name, email, nationality, gender) VALUES
-    ('Rayan fiache', 'Rayan3@example.com', 'Morocco', 'Male'),
-    ('Jane Doe', 'jane@example.com', 'Canada', 'Female')";
-
-if ($conn->query($insertDataSql) === TRUE) {
-    echo "Sample data inserted successfully";
-} else {
-    echo "Error inserting sample data: " . $conn->error;
-}
-
+// Do not close the connection here; let the including script close it.
+// $conn->close();
 ?>
