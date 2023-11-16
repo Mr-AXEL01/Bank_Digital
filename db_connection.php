4e268f3f1687aa984ff393dbc28bcc20956cbeba
abdelhak20azur02@gmail.com <abdelhak20azur02@gmail.com>
2023-11-16 03:23:33 +0100

    creat a customers table in our database

--- db_connection.php
+++ db_connection.php
@@ -15,5 +15,15 @@ if ($conn->connect_error) {
 // select our database 
 $conn->select_db($dbname);
 
+// Create customers table if not exists
+$sql = "CREATE TABLE IF NOT EXISTS customers (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    nationality VARCHAR(50),
+    gender VARCHAR(10),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+)";
+
 
 ?>
